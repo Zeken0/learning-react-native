271d66b9b90a663c6156e7faca6ad37b86aa47c6
Ahmed  Jibril <Ahmed.j.jibril@hotmail.com>
2022-11-24 14:36:08 +0100

    still learning

---
+++ app/screens/ViewImageScreen.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Image, StyleSheet, View } from "react-native";
+import colors from "../config/colors";
+
+function ViewImageScreen(props) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.closeIcone}></View>
+      <View style={styles.deleteIcone}></View>
+      <Image
+        style={styles.image}
+        source={require("../assets/chair.jpg")}
+        resizeMode="contain"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.black,
+    flex: 1,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  closeIcone: {
+    width: 50,
+    height: 50,
+    backgroundColor: colors.primary,
+    position: "absolute",
+    top: 40,
+    left: 30,
+  },
+  deleteIcone: {
+    width: 50,
+    height: 50,
+    backgroundColor: colors.secondary,
+    position: "absolute",
+    top: 40,
+    right: 30,
+  },
+});
+
+export default ViewImageScreen;
